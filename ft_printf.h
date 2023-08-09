99d28b6718b0faa69f5d152751306b08db17a178
ajakob <ajakob@student.42heilbronn.de>
2023-08-09 15:10:14 +0200

    Commit

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ajakob <ajakob@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 16:41:52 by ajakob            #+#    #+#             */
-/*   Updated: 2023/01/04 02:15:20 by ajakob           ###   ########.fr       */
+/*   Updated: 2023/08/09 15:09:58 by ajakob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int		ft_printf(const char *s, ...);
 int		ft_putchar(char s);
 int		ft_putstr(char *s);
 int		ft_putnbr(int n);
-char	*ft_uitoa(unsigned int n);
 int		ft_putunbr(unsigned int n);
 int		ft_puthexa(char c, unsigned int p);
 int		ft_print_mem(char s, unsigned long p);
