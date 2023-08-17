5b3b3e73d46dce4fc31935c0fddc4d17e743b7e5
ajakob <ajakob@student.42heilbronn.de>
2023-08-17 17:51:49 +0200

    Updated Makefile

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -1,29 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ajakob <ajakob@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/26 16:41:52 by ajakob            #+#    #+#             */
-/*   Updated: 2023/08/09 15:09:20 by ajakob           ###   ########.fr       */
+/*   Created: 2023/08/09 20:12:09 by ajakob            #+#    #+#             */
+/*   Updated: 2023/08/09 20:17:02 by ajakob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	int				i;
 
-# include <stdarg.h>
-# include "libft/libft.h"
-
-int		ft_printf(const char *s, ...);
-int		ft_putchar(char s);
-int		ft_putstr(char *s);
-int		ft_putnbr(int n);
-int		ft_putunbr(unsigned int n);
-int		ft_puthexa(char c, unsigned int p);
-int		ft_print_mem(char s, unsigned long p);
-void	print_order(char s, unsigned long p);
-int		hex_ulong_len(unsigned long p);
-
-#endif
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (str1[i] && str2[i])
+	{
+		if ((str1[i] != str2[i]))
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	if (str1[i] || str2[i])
+		return (str1[i] - str2[i]);
+	return (0);
+}
